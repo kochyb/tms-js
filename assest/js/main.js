746326df5ab09daf007eaaef80ebfa7645b66360
kochyb <denisich007@mail.ru>
2020-01-07 16:58:23 +0300

    Lesson 3 Home Work

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,17 +1,39 @@
-const data = {
-    firstName: 'Hannah',
-    lastName: 'Dakota',
-    age: 24,
-};
+const data = [
+    {
+        firstName: 'Ashton',
+        lastName: 'Kutcher',
+        age: 40,
+    },
+    {
+        firstName: 'Bradley',
+        lastName: 'Pitt',
+        age: 54,
+    },
+    {
+        firstName: 'Hannah',
+        lastName: 'Dakota',
+        age: 24,
+    },
+];
 
+
+let outputName;
 function searchFun() {
     let fname = document.getElementById('first_name').value;
-    if (fname.toUpperCase() === data.firstName.toUpperCase()) {
-        document.getElementById('result').innerHTML = 'Имя: ' + data.firstName + '<br>Фамилия:  ' + data.lastName + '<br>Возраст: ' + data.age;
-    }
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].firstName.toUpperCase() == fname.toUpperCase()) {
+            outputName = 'Имя: ' + data[i].firstName + '<br>Фамилия: ' + data[i].lastName + '<br>Возраст: ' + data[i].age;
+            result.innerHTML = outputName;
+            console.log(outputName);
+            break
+        }
     else {
-        document.getElementById('result').innerHTML = 'No results found for your request';
+            result.innerHTML = 'No results found for your request';
+    }
     }
 }
 
 document.getElementById('search').addEventListener('click', searchFun);
+
+
+
